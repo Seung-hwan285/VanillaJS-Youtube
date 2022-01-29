41e0b1291e23fd181f867e8feadac583969d245b
Seung-hwan285 <ghks285@naver.com>
2022-01-29 14:47:57 +0900

    feat : 검색했을때 로딩 컴포넌트와 동영상 올바르게 출력

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import {$} from "./utils/querySelector.js";
-import {onModalClose, onModalShowOpen, renderLoadingComponent, renderLoadingVideo} from "./view/Modalview.js";
+import {onModalClose, onModalShowOpen} from "./view/Modalview.js";
 import {onModelShow} from "./handle/onModalShow.js";
 
 
@@ -8,8 +8,8 @@ export const YoutubeClassRoom =()=>{
   $('#search-button').addEventListener("click",onModalShowOpen);
   $('.modal-close').addEventListener("click",onModalClose);
   $('#data-submit').addEventListener("click",onModelShow);
-  renderLoadingComponent();
-  renderLoadingVideo();
+
+
 };
 
 
--- src/js/view/Modalview.js
+++ src/js/view/Modalview.js
@@ -36,32 +36,30 @@ export const renderRecentKeyword=(keyword)=>{
 };
 
 
-// 1. 검색 버튼 누르면 로딩 컴포넌트가 나와야한다.
-// -> video-wrapper 부분 건들면 될 것 같음
-// -> clip 부분 따로 빼서 video-wrappe로 로딩컴포넌트 보여주고 나오게 해보자
 
 // 2. 로딩 컴포넌트가 생기다가 2초정도 그리고 해당 검색 영상들이 나온다.
-
+// -> 1초뒤에 로딩 컴포넌트 나오고있음
+// -> 3초뒤에 로딩 컴포넌트 제거
+// -> 해당 영상 출력
 export const renderLoadingComponent=()=>{
 
     const $spinner = $('[data-youtube="search-modal_spinner"]');
-
-
-
-  setTimeout(function (){
-      $spinner.innerHTML=`<div class="relative spinner-container">
-        <span class="material spinner"></span></div>`;
-  },2000);
-
-
-
-
-}
-
-export const renderLoadingVideo=()=>{
     const $video = $('[data-youtube="search-video"]');
 
-    $video.innerHTML=`        <article class="clip">
+
+    setTimeout(function (){
+      $spinner.innerHTML=`<div class="relative spinner-container">
+        <span class="material spinner"></span></div>`;
+  },1000);
+
+
+    setTimeout(function () {
+        $spinner.innerHTML=``;
+    },3000);
+
+
+    setTimeout(function () {
+        $video.innerHTML=`        <article class="clip">
                 <div class="preview-container">
                   <iframe
                     width="100%"
@@ -91,4 +89,13 @@ export const renderLoadingVideo=()=>{
                   </div>
                 </div>
               </article>`;
+    },3000);
+
+
+
+}
+
+export const renderLoadingVideo=()=>{
+
+
 }

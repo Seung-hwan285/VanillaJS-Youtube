07752015c9c7997c37aeee8810f9fd19971b855b
Seung-hwan285 <ghks285@naver.com>
2022-02-12 14:24:51 +0900

    fix : 저장개수 1개부족한 현상

--- src/js/handle/onModalSave.js
+++ src/js/handle/onModalSave.js
@@ -16,7 +16,7 @@ export const onModalSave=(e)=>{
     const parseVideo = JSON.parse(getvideos);
     const saveBox = $('#save');
 
-
+    saveBox.innerHTML=`저장된 영상 갯수 : ${parseVideo.length}개`
 
 
     // 로컬스토리지 저장갯수가 5개 이상이거나 5개이면 에러메세지 표시하고 더이상 저장안되게

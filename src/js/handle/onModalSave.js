9764c25a996f37435d4f0c46363a762ea9223561
Seung-hwan285 <ghks285@naver.com>
2022-02-12 12:12:14 +0900

    refactor : 로컬스토리지 모듈 분리

--- src/js/handle/onModalSave.js
+++ src/js/handle/onModalSave.js
@@ -5,55 +5,20 @@
 // [x]  저장개수가 초과하면 더이상 저장할수 없다고 에러메세지
 
 
-import {STORAGE_FULL} from "../constant.js";
-import {$} from "../utils/querySelector.js";
 
-const videoList =[];
+import {getVideo, handlerVideoData, setVideo} from "../utils/localStorage.js";
 
 export const onModalSave=(e)=>{
 
-    const target = e.target;
-
-
-    const video ={
-        videoId : target.dataset.videoId,
-        videoTitle : target.dataset.videoTitel,
-        channelId : target.dataset.channelId,
-        channelTitle : target.dataset.channelId,
-        publishedAt : target.dataset.publishedAt,
-    }
-
-    videoList.push(video);
-
+    handlerVideoData(e);
     setVideo();
     getVideo();
 
 }
 
-const setVideo=()=>{
-    localStorage.setItem("videoWatch",JSON.stringify(videoList));
-}
 
 
-const getVideo=()=>{
 
-    const getvideos=localStorage.getItem("videoWatch");
-    const localFull = $('#storage-full');
-    const saveBox = $('#save');
-    if(getvideos !==null){
-        const paresVideo =JSON.parse(getvideos);
-        console.log(paresVideo.length);
 
-        saveBox.innerHTML=`저장된 영상 갯수 : ${paresVideo.length}개`;
-        if(paresVideo.length >=5){
 
-            // 에러메시지 창
-            localFull.innerText=STORAGE_FULL;
-            localFull.classList.add('show');
 
-            setTimeout(()=>{
-               localFull.classList.remove('show');
-            },2000);
-        }
-    }
-}
---
+++ src/js/utils/localStorage.js
@@ -0,0 +1,56 @@
+import {$} from "./querySelector.js";
+import {STORAGE_FULL} from "../constant.js";
+
+const localFull = $('#storage-full');
+const videoList =[];
+
+const classListToAdd =()=>{
+    localFull.classList.add('show');
+}
+
+const classListToRemove =()=>{
+    setTimeout(()=>{
+        localFull.classList.remove('show');
+    },2000);
+}
+
+export const setVideo=()=>{
+    localStorage.setItem("videoWatch",JSON.stringify(videoList));
+}
+
+export const getVideo=()=>{
+
+    const getvideos=localStorage.getItem("videoWatch");
+    const saveBox = $('#save');
+
+    if(getvideos !==null){
+        const paresVideo =JSON.parse(getvideos);
+        console.log(paresVideo.length);
+
+        saveBox.innerHTML=`저장된 영상 갯수 : ${paresVideo.length}개`;
+        if(paresVideo.length >=5){
+            classListToAdd();
+            // 에러메시지 창
+            localFull.innerText=STORAGE_FULL;
+            classListToRemove();
+        }
+    }
+}
+
+export const handlerVideoData =(e)=>{
+
+    const target = e.target;
+
+
+    const video ={
+        videoId : target.dataset.videoId,
+        videoTitle : target.dataset.videoTitel,
+        channelId : target.dataset.channelId,
+        channelTitle : target.dataset.channelId,
+        publishedAt : target.dataset.publishedAt,
+    }
+
+    videoList.push(video);
+}
+
+
